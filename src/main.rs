32222c0bc730bc2252efc1118fd60e540ea724ae
Sl33pySage <52090741+Sl33pySage@users.noreply.github.com>
2026-06-06 16:32:32 -0500

    App chapter/app (#2)
    * On Helper functions
    
    * on toggle_editing()
    
    * On chapt main now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::crossterm::event::EnableMouseCapture;
+use ratatui::crossterm::execute;
+use ratatui::crossterm::terminal::{EnterAlternateScreen, enable_raw_mode};
+use std::io;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stderr = io::stderr(); // This is a special case. Normally using stdout is fine
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+    Ok(())
 }
